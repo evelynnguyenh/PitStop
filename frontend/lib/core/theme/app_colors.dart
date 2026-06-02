6e853451d519d11bba13b95860768f1968bf70bd
tungnguyen712 <nguyenductung712@gmail.com>
2026-06-01 23:25:44 -0700

    set theme colors

---
+++ frontend/lib/core/theme/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color primary = Color(0xFFFFD65A);
+  static const Color red = Color(0xFFCA3131);
+  static const Color green = Color(0xFF34A853);
+}
